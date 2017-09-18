031937af8a16c44aa0c3d589a6cce079871528e4
RodrigoSantosCosta <31769425+RodrigoSantosCosta@users.noreply.github.com>
2017-09-18 14:46:29 -0300

    Update SMPSEQ3.java

--- Exercicios/SMPSEQ3.java
+++ Exercicios/SMPSEQ3.java
@@ -1,20 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.util.Scanner;
 
-/**
- *
- * @author aluno
- */
 class SMPSEQ3 {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
             int n,m;
 
